ebb53c5242b74106a0f19f7cb268b8964103880d
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2014-03-14 15:53:01 +0000

    Initial SignalR client type provider

---
+++ SignalRProvider/SignalRProvider.fs
@@ -0,0 +1,48 @@
+﻿module SignalRProvider
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+
+[<TypeProvider>]
+type ClientProvider (config: TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+    let ns = "SignalRProvider.Provided"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
+
+    // TODO - use all?
+    let clientAsm =
+        let lastref = config.ReferencedAssemblies |> Seq.last
+        let refAsm = Assembly.LoadFrom lastref
+        refAsm
+
+    let hubs = 
+        clientAsm.DefinedTypes
+        |> Seq.filter (fun t -> t.GetCustomAttributes<Microsoft.AspNet.SignalR.Hubs.HubNameAttribute>() |> Seq.isEmpty |> not )
+
+    let hubName (hubType : TypeInfo) =
+        hubType.GetCustomAttribute<Microsoft.AspNet.SignalR.Hubs.HubNameAttribute>().HubName
+
+    let makeHubType hubType =
+        let name = hubName hubType
+        let props = 
+            hubType.DeclaredProperties
+            |> Seq.map (fun p -> p.Name)
+            |> Seq.map (fun p -> ProvidedProperty(name, typeof<string>, IsStatic = true, GetterCode = (fun args -> <@@ "Hello world " @@>)))
+        let ty = ProvidedTypeDefinition(asm, ns, name, Some typeof<obj>)
+        props |> Seq.iter (fun prop -> ty.AddMember prop)
+        ty
+
+    let hubTypes = 
+        hubs
+        |> Seq.map makeHubType
+        |> Seq.toList
+
+    do
+        this.AddNamespace(ns, hubTypes)
+
+[<assembly:TypeProviderAssembly>]
+do ()
