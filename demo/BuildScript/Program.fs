41614e87364d935ab701690c626a87738684103c
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2015-02-06 07:45:53 +0000

    Cleanup

--- demo/BuildScript/Program.fs
+++ demo/BuildScript/Program.fs
@@ -1,11 +1,8 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-open System.IO
+﻿open System.IO
 
 [<EntryPoint>]
 let main argv = 
     let script = (new SignalRClient.Wrapper()).GenerateScript()
-    File.WriteAllText("/Users/nicholaw/Sites/Scripts/SignalRFunScriptClient.js", script)
+    File.WriteAllText("SignalRFunScriptClient.js", script)
     0 // return an integer exit code
 
